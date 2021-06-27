d05a9fff9f23b313e32eaf3f0b472e4cd8beafce
JimmyMejiaaa <85595052+JimmyMejiaaa@users.noreply.github.com>
2021-06-27 16:54:51 -0500

    Update Ejercicio 5, Horas extras.py

--- Ejercicio 5, Horas extras.py
+++ Ejercicio 5, Horas extras.py
@@ -1,3 +1,6 @@
+#6.	Determinar la cantidad de dinero que recibirá un trabajador por concepto de las horas extras trabajadas en una empresa, sabiendo que cuando las horas de trabajo 
+# exceden de 40, el resto  se consideran horas extras y que éstas se pagan al doble de una hora normal cuando no exceden de 8;  si las horas extras exceden de 8 se 
+# pagan las primeras 8 al  doble de lo que se paga por una hora normal y el resto al triple.
 class HExtras:
     def __init__(self):
         pass
